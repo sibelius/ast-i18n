74aa87c6e737f54ed481324f6f7220e9497bc304
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2019-02-10 12:27:14 -0200

    fix(i18n): do not import twice

---
+++ src/__testfixtures__/Imported.input.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+
+const Simple = () => (
+  <span>My simple text</span>
+);
---
+++ src/__testfixtures__/Imported.output.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+
+const Simple = () => (
+  <span>{t('my_simple_text')}</span>
+);
