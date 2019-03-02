f5c36332804515813df2f3819f74ce9342495ce6
Arminas Katilius <katilius.arminas@gmail.com>
2019-03-02 13:53:22 +0200

    Split code into smaller functions

--- src/i18nTransformerCodemod.ts
+++ src/i18nTransformerCodemod.ts
@@ -1,7 +1,9 @@
 import { API, FileInfo, Options, JSCodeshift, Collection } from 'jscodeshift';
 import { getStableKey } from './stableString';
 import { hasStringLiteralArguments, hasStringLiteralJSXAttribute } from './visitorChecks';
-import { identifier } from "@babel/types";
+import { CallExpression, ImportDeclaration, JSXAttribute, JSXText } from "@babel/types";
+import { NodePath } from "@babel/traverse";
+import { JSXExpressionContainer } from "ast-types/gen/nodes";
 
 const tCallExpression = (j: JSCodeshift, key: string) => {
   return j.callExpression(
@@ -22,14 +24,14 @@ const getImportStatement = (useHoc: boolean = true, useHooks: boolean = false) =
   return `import { useTranslation, withTranslation } from 'react-i18next';`;
 };
 
-const addI18nImport = (j: JSCodeshift, root: Collection<any>, {useHooks, useHoc}) => {
+const addI18nImport = (j: JSCodeshift, root: Collection<any>, {useHooks, useHoc}: any) => {
   // TODO - handle hoc or hooks based on file
   const statement = getImportStatement(useHoc, useHooks);
 
   // check if there is a react-i18next import already
   const reactI18nNextImports = root
     .find(j.ImportDeclaration)
-    .filter(path => path.node.source.value === 'react-i18next');
+    .filter((path : NodePath<ImportDeclaration>) => path.node.source.value === 'react-i18next');
 
   if (reactI18nNextImports.length > 0) {
     return;
@@ -60,51 +62,99 @@ function transform(file: FileInfo, api: API, options: Options) {
 
   let hasI18nUsage = false;
 
-  //<span>test</span>
-  root
-    .find(j.JSXText)
-    .forEach(path => {
-      const key = getStableKey(path.node.value);
+  hasI18nUsage = translateJsxContent(j, root) || hasI18nUsage;
+  hasI18nUsage = translateJsxProps(j, root) || hasI18nUsage;
+  hasI18nUsage = translateFunctionArguments(j, root) || hasI18nUsage;
 
-      // TODO - use j.jsxExpressionContainer
-      if (path.node.value && path.node.value.trim()) {
-        path.node.value = `{t('${key}')}`;
-        hasI18nUsage = true;
+  if (hasI18nUsage) {
+    // export default withTranslation()(Component)
+    let hooksUsed = false;
+    let hocUsed = false;
+    root
+      .find(j.ExportDefaultDeclaration)
+      .filter(path => {
+        return path.node.declaration.type === 'Identifier' || path.node.declaration.type === 'CallExpression';
+      })
+      .forEach(path => {
+        if (path.node.declaration.type === 'Identifier') {
+          const exportedName = path.node.declaration.name;
+          const functions = findFunctionByIdentifier(j, exportedName, root);
+          let hookFound = addUseHookToFunctionBody(
+            j, functions
+          );
+
+          if(!hookFound) {
+            hocUsed = true;
+            path.node.declaration = withTranslationHoc(j, j.identifier(path.node.declaration.name));
+          } else {
+            hooksUsed = true;
+          }
+          return;
+        }
+
+        if (path.node.declaration.type === 'CallExpression') {
+          if (path.node.declaration.callee.name === 'withTranslate') {
+            return;
+          }
+
+          path.node.declaration.arguments.forEach(identifier => {
+            const functions = findFunctionByIdentifier(j, identifier.name, root);
+            hooksUsed = addUseHookToFunctionBody(j, functions) || hooksUsed;
+          });
+
+          if (!hooksUsed) {
+            hooksUsed = true;
+            path.node.declaration = withTranslationHoc(j, path.node.declaration);
+          }
+        }
+      });
+
+    addI18nImport(j, root, {useHooks: hooksUsed, useHoc: hocUsed});
+    // print
+    return root.toSource(printOptions);
+  }
+}
+
+function useTranslateHook(j: JSCodeshift) {
+  return j.variableDeclaration('const',
+    [j.variableDeclarator(
+      j.identifier('{ t }'),
+      j.identifier('useTranslation()')
+    )]
+  );
+}
+
+function findFunctionByIdentifier(j: JSCodeshift, identifier: string, root: Collection<any>) {
+  return root.find(j.Function)
+    .filter((p: NodePath<Function>) => {
+      if (j.FunctionDeclaration.check(p.node)) {
+        return p.node.id.name === identifier;
       }
+      return p.parent.value.id && p.parent.value.id.name === identifier;
     });
+}
 
-  //<Comp name='Awesome' />
-  root
-    .find(j.JSXAttribute)
-    .filter(path => hasStringLiteralJSXAttribute(path))
-    .forEach(path => {
-      const key = getStableKey(path.node.value.value);
-      hasI18nUsage = true;
-
-      path.node.value = j.jsxExpressionContainer(
-        tCallExpression(j, key),
-      );
+function addUseHookToFunctionBody(j: JSCodeshift, functions: Collection<any>) {
+  let hookFound = false;
+    functions
+    .every(n => {
+      hookFound = true;
+      const body = n.node.body;
+      n.node.body =  j.BlockStatement.check(body)
+        ? j.blockStatement([useTranslateHook(j), ...body.body])
+        : j.blockStatement([useTranslateHook(j), j.returnStatement(body)])
     });
+  return hookFound;
+}
 
-  //<Comp name={'Awesome'} />
-  root
-    .find(j.JSXExpressionContainer)
-    .filter(path => {
-      return path.node.expression && path.node.expression.type === 'StringLiteral'
-    })
-    .forEach(path => {
-      const key = getStableKey(path.node.expression.value);
-      hasI18nUsage = true;
-
-      path.node.expression = tCallExpression(j, key);
-    });
-
-  // Yup.string().required('this field is required')
-  // showSnackbar({ message: 'ok' })
+// Yup.string().required('this field is required')
+// showSnackbar({ message: 'ok' })
+function translateFunctionArguments(j: JSCodeshift, root: Collection<any>) {
+  let hasI18nUsage = false;
   root
     .find(j.CallExpression)
-    .filter(path => hasStringLiteralArguments(path))
-    .forEach(path => {
+    .filter((path: NodePath<CallExpression>) => hasStringLiteralArguments(path))
+    .forEach((path: NodePath<CallExpression>) => {
       if (hasStringLiteralArguments(path)) {
         path.node.arguments = path.node.arguments.map(arg => {
           if (arg.type === 'StringLiteral' && arg.value) {
@@ -131,101 +181,67 @@ function transform(file: FileInfo, api: API, options: Options) {
       }
     });
 
-  if (hasI18nUsage) {
-    // export default withTranslation()(Component)
-    let hooksUsed = false;
-    let hocUsed = false;
-    root
-      .find(j.ExportDefaultDeclaration)
-      .filter(path => {
-        return path.node.declaration.type === 'Identifier' || path.node.declaration.type === 'CallExpression';
-      })
-      .forEach(path => {
-        if (path.node.declaration.type === 'Identifier') {
-          const exportedName = path.node.declaration.name;
-          const functions = findFunctionByIdentifier(j, exportedName, root);
-          let hookFound = addUseHookToFunctionBody(
-            j, functions
-          );
-
-          if(!hookFound) {
-            hocUsed = true;
-            path.node.declaration = j.callExpression(
-              j.callExpression(
-                j.identifier('withTranslation'),
-                [],
-              ),
-              [
-                j.identifier(path.node.declaration.name),
-              ],
-            );
-          } else {
-            hooksUsed = true;
-          }
-          return;
-        }
-
-        if (path.node.declaration.type === 'CallExpression') {
-          if (path.node.declaration.callee.name === 'withTranslate') {
-            return;
-          }
-
-          path.node.declaration.arguments.forEach(identifier => {
-            const functions = findFunctionByIdentifier(j, identifier.name, root);
-            hooksUsed = hooksUsed || addUseHookToFunctionBody(j, functions);
-          });
-
-          if (!hooksUsed) {
-            hooksUsed = true;
-            path.node.declaration = j.callExpression(
-              j.callExpression(
-                j.identifier('withTranslation'),
-                [],
-              ),
-              [
-                path.node.declaration,
-              ],
-            );
-          }
-        }
-      });
-
-    addI18nImport(j, root, {useHooks: hooksUsed, useHoc: hocUsed});
-    // print
-    return root.toSource(printOptions);
-  }
+  return hasI18nUsage;
 }
 
-function useTranslateHook(j: JSCodeshift) {
-  return j.variableDeclaration('const',
-    [j.variableDeclarator(
-      j.identifier('{ t }'),
-      j.identifier('useTranslation()')
-    )]
-  );
-}
+//<span>test</span>
+function translateJsxContent(j: JSCodeshift, root: Collection<any>) {
+  let hasI18nUsage = false;
+  root
+    .find(j.JSXText)
+    .forEach((path: NodePath<JSXText>) => {
+      const key = getStableKey(path.node.value);
 
-function findFunctionByIdentifier(j: JSCodeshift, identifier: string, root: Collection<any>) {
-  return root.find(j.Function)
-    .filter(p => {
-      if (j.FunctionDeclaration.check(p.node)) {
-        return p.node.id.name === identifier;
+      // TODO - use j.jsxExpressionContainer
+      if (path.node.value && path.node.value.trim()) {
+        path.node.value = `{t('${key}')}`;
+        hasI18nUsage = true;
       }
-      return p.parent.value.id && p.parent.value.id.name === identifier;
     });
+  return hasI18nUsage;
 }
 
-function addUseHookToFunctionBody(j: JSCodeshift, functions: Collection<any>) {
-  let hookFound = false;
-    functions
-    .every(n => {
-      hookFound = true;
-      const body = n.node.body;
-      n.node.body =  j.BlockStatement.check(body)
-        ? j.blockStatement([useTranslateHook(j), ...body.body])
-        : j.blockStatement([useTranslateHook(j), j.returnStatement(body)])
+function translateJsxProps(j: JSCodeshift, root: Collection<any>) {
+  let hasI18nUsage = false;
+  //<Comp name='Awesome' />
+  root
+    .find(j.JSXAttribute)
+    .filter((path: NodePath<JSXAttribute>) => hasStringLiteralJSXAttribute(path))
+    .forEach((path: NodePath<JSXAttribute>) => {
+      const key = getStableKey(path.node.value.value);
+      hasI18nUsage = true;
+
+      path.node.value = j.jsxExpressionContainer(
+        tCallExpression(j, key),
+      );
     });
-  return hookFound;
+
+  //<Comp name={'Awesome'} />
+  root
+    .find(j.JSXExpressionContainer)
+    .filter((path: NodePath<JSXExpressionContainer>) => {
+      return path.node.expression && path.node.expression.type === 'StringLiteral'
+    })
+    .forEach(path => {
+      const key = getStableKey(path.node.expression.value);
+      hasI18nUsage = true;
+
+      path.node.expression = tCallExpression(j, key);
+    });
+
+  return hasI18nUsage;
+}
+
+function withTranslationHoc(j: JSCodeshift, identifier: any) {
+  return j.callExpression(
+    j.callExpression(
+      j.identifier('withTranslation'),
+      [],
+    ),
+    [
+      identifier
+    ],
+  )
 }
 
 
