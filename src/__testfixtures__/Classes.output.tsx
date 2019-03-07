777a2cf24f008a71e59ab4ab4008e5f4e8efa0ec
Arminas Katilius <katilius.arminas@gmail.com>
2019-03-07 08:40:25 +0200

    Add t call from props for class components

--- src/__testfixtures__/Classes.output.tsx
+++ src/__testfixtures__/Classes.output.tsx
@@ -4,6 +4,7 @@ import { withTranslation } from 'react-i18next';
 
 class MyClass extends React.Component {
   render() {
+    const { t } = this.props;
     return (
       <div>
         <span>{t('my_great_class_component')}</span>
